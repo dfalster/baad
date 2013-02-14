c42dcd99d17ce282348a8f8b0bf77d65d5e80341
Daniel Falster <daniel.falster@mq.edu.au>
2013-02-15 10:29:57 +1100

    Renamed pdfs to same as directory

---
+++ sh/renamePdf.sh
@@ -0,0 +1,8 @@
+# Copy import.csv into all data directories
+cd data
+for filename in *
+do
+   echo $filename.pdf
+   mv $filename/*.pdf $filename/$filename.pdf
+#   mv $filename/import.txt $filename/import.csv
+done
