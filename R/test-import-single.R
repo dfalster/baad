3b6b52c3099a31d9cb14aca3956313b44111f999
Daniel Falster <daniel.falster@mq.edu.au>
2013-02-20 10:53:01 +1100

    Add test script for importing single study

---
+++ R/test-import-single.R
@@ -0,0 +1,13 @@
+
+library(testthat)
+rm(list=ls())
+source('R/Biomass-fun.R')
+source('R/import-fun.R')
+source("R/test-import-fun.R")
+
+#Test single study
+studyName     <-  "Aiba2005"
+system(paste0("rm output/data/", studyName, ".csv"))
+data<-importData(studyName)
+compareOldNew(studyName)
+cat("\n")
