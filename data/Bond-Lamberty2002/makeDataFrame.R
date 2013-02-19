1cc289329316ff9ba2199d5720657dc31601ea45
Daniel Falster <daniel.falster@mq.edu.au>
2013-02-19 14:05:30 +1100

    journal update. Moved incomplete R file to R-working.

--- data/Bond-Lamberty2002/makeDataFrame.R
+++ data/Bond-Lamberty2002/makeDataFrame.R
@@ -7,6 +7,18 @@ makeDataFrame<-function(raw, studyName){
   raw$Species[raw$Species=='JP']  <-  "Pinus banksiana"
   raw$Species[raw$Species=='T']   <-  "Larix laricina"
   raw$Species[raw$Species=='W']   <-  "Salix spp"
-  data   <-  cbind(dataset=studyName, grouping=paste(raw$Site, raw$HarvestYear, raw$Edaphic, sep="; "), species=raw$Species, age=raw$HarvestYear-raw$BurnYear, stemMass=raw$TotBranch+raw$Stem, raw[,c("Height", "D0", "DBH", "TotFol", "Sapwood", "Root")], longitude=raw$longitude, latitude=raw$latitude, growingCondition="FW", vegetation="BorF", location="Near Thompson and Leaf Rapids, Manitoba, Canada", reference="Bond-Lamberty B, Wang C, Gower ST (2002) Aboveground and belowground biomass and sapwood area allometric equations for six boreal tree species of northern Manitoba. Canadian Journal of Forest Research 32:1441–1450.", stringsAsFactors=FALSE)
+  data   <-  cbind(dataset=studyName, 
+                   grouping=paste(raw$Site, raw$HarvestYear, raw$Edaphic, sep="; "), 
+                   species=raw$Species, 
+                   age=raw$HarvestYear-raw$BurnYear, 
+                   stemMass=raw$TotBranch+raw$Stem, 
+                   raw[,c("Height", "D0", "DBH", "TotFol", "Sapwood", "Root")], 
+                   longitude=raw$longitude, 
+                   latitude=raw$latitude, 
+                   growingCondition="FW", 
+                   vegetation="BorF", 
+                   location="Near Thompson and Leaf Rapids, Manitoba, Canada", 
+                   reference="Bond-Lamberty B, Wang C, Gower ST (2002) Aboveground and belowground biomass and sapwood area allometric equations for six boreal tree species of northern Manitoba. Canadian Journal of Forest Research 32:1441–1450.", 
+                   stringsAsFactors=FALSE)
 
 }
