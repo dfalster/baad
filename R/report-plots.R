e85f5a4149925f475a80f8c92f680e2e48929247
dbarneche <diego.barneche@ymail.com>
2013-04-24 15:25:50 +1000

    report-plots and report-plots-fun added and working

---
+++ R/report-plots.R
@@ -0,0 +1,16 @@
+rm(list=ls())
+source('R/Biomass-fun.R')
+source('R/import-fun.R')
+source('R/report-plots-fun.R')
+source('R/packages.R')
+
+#names of all studies
+studyNames     <-  getStudyNames()
+
+#import data
+d  <-  addStudies(studyNames, reprocess = FALSE, verbose = FALSE)
+
+#Explore
+alldata  <-  d$data
+
+comparePlots(alldata, pdf=TRUE)
