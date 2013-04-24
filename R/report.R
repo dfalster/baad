0656ed7cc3778ae006ea949f92a0ebdaa7e47dfd
dbarneche <diego.barneche@ymail.com>
2013-04-24 12:05:15 +1000

    fixed and cleaned the recovered version of report.R.

--- R/report.R
+++ R/report.R
@@ -1,4 +1,3 @@
-
 rm(list=ls())
 source('R/Biomass-fun.R')
 source('R/import-fun.R')
@@ -9,23 +8,7 @@ source('R/packages.R')
 studyNames     <-  getStudyNames()
 
 #import data
-d<-addStudies(studyNames, reprocess = FALSE, verbose = FALSE)
-
-#plots for all data
-makePlotPanel(d$data, "all", col=niceColors()[as.factor(d$data$dataset)], pdf=TRUE)
-
-#plots for each study
-makePlotPanel(d$data, "Ishihara0000", pdf=FALSE) #one study, print to screen
-tmp<-lapply(unique(d$data$dataset), makePlotPanel, data=d$data, dir="report")  #all, print to file
-
-#maps for each study
-makeMapPlot(data=d$data, study="Aiba2005", pdf=TRUE) #one study, print to screen
-tmp2<-lapply(unique(d$data$dataset), makeMapPlot, data=d$data, dir="report", pdf=TRUE)  #all, print to file
-
-#print report fo study
-studyReport(d, "Ishihara0000")
-
-writeEmail(d)
+d  <-  addStudies(studyNames, reprocess = FALSE, verbose = FALSE)
 
 #create dataNew.csv files for authors to fill in
 z  <-  lapply(studyNames, function(x){generateDataNew(d$data, x)})
@@ -33,14 +16,8 @@ z  <-  lapply(studyNames, function(x){generateDataNew(d$data, x)})
 #for each study, put variable.definition, contactInfo and Reference in the email folder
 y  <-  lapply(studyNames, function(x){emailFiles(d$data, x)})
 
-## NEW: markdown report.
-# Need package 'knitr' installed (added to packages.R; RAD)
-studyReportMd(d, "Ishihara0000")
-studyReportMd(d, "Aiba2007")
-studyReportMd(d, "Bond-Lamberty2002")
+#markdown report: needs package 'knitr' installed (added to packages.R; RAD)
+studyReportMd(d, "Ilomaki2003")
+studyReportMd(d, "Parviainen1999")
 
 l  <-  lapply(studyNames, function(x){studyReportMd(d, x)})
-
-##
-writeLines(sprintf('%s,', getContributors(d)$name))
-
