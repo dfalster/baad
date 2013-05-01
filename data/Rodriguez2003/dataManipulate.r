42f75789c36da3c9159819ced464c1eac8f2ee9b
dbarneche <diego.barneche@ymail.com>
2013-05-01 11:43:25 +1000

    removed the average SLA measure from Rodriguez2003. The calculation in dataManipulate seemed not to work.

--- data/Rodriguez2003/dataManipulate.r
+++ data/Rodriguez2003/dataManipulate.r
@@ -3,6 +3,3 @@ raw[["Crown_class"]][raw[["Crown_class"]]==1]  <-  "intermediate"
 raw[["Crown_class"]][raw[["Crown_class"]]==1]  <-  "codominant"
 raw[["Crown_class"]][raw[["Crown_class"]]==1]  <-  "dominant"
 raw$grouping                         <-  paste("Treatment", raw[["Treatment"]], raw[["Crown_class"]], sep="; ")
-
-#calculate average sla
-raw$sla <-(raw[["SLA_Lower_third"]]+raw[["SLA_Middle_Third"]]+raw[["SLA_Upper_third"]])/3
