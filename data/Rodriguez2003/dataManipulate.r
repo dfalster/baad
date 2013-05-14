217d83c0b7791f42ee588c9359070f4b89c1c9b5
dbarneche <diego.barneche@ymail.com>
2013-05-15 09:21:44 +1000

    fixed grouping and status variables in Rodriguez2003

--- data/Rodriguez2003/dataManipulate.r
+++ data/Rodriguez2003/dataManipulate.r
@@ -1,9 +1,5 @@
 manipulate <- function(raw) {
   raw[["Bark_Stemwood"]]                    <-  raw[["Bark"]] + raw[["Stemwood"]]
-  raw[["Crown_class"]][raw[["Crown_class"]]==1]  <-  "intermediate"
-  raw[["Crown_class"]][raw[["Crown_class"]]==1]  <-  "codominant"
-  raw[["Crown_class"]][raw[["Crown_class"]]==1]  <-  "dominant"
-  raw$grouping                         <-  makeGroups(raw, c("Treatment", "Crown_class"))
   raw
 }
 
