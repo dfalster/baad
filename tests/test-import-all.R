ed850a00dd2f2adc9f408ed480e155555b9fc15e
Daniel Falster <daniel.falster@mq.edu.au>
2013-05-14 18:17:30 +1000

    Update test script ot load all studies

--- tests/test-import-all.R
+++ tests/test-import-all.R
@@ -1,17 +1,6 @@
-
-library(testthat)
 rm(list=ls())
-source('R/Biomass-fun.R')
+source('R/packages.R')
 source('R/import-fun.R')
-source("R/test-import-fun.R")
 
-#Test single study
-#d<-importAndCheck("O'Hara0000", verbose=TRUE)
-#names of all studies
-studyNames     <-  getStudyNames()
-
-#import data
-d<-addStudies(studyNames, reprocess = FALSE, verbose = TRUE)
-#d<-addStudies(studyNames, reprocess = TRUE, verbose = TRUE)
-#d<-addStudies(studyNames[-c(2)])
-#d<-addStudies(studyNames[2], data=d)
+# Load all data
+dat <- loadStudies(reprocess=TRUE)
