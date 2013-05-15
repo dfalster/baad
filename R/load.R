d679a3e85cebece07a0b1d18fc95fc0cda56322d
dbarneche <diego.barneche@ymail.com>
2013-05-15 16:08:24 +1000

    fixed load.R to incorporate new studies that need files to be set up

--- R/load.R
+++ R/load.R
@@ -2,4 +2,11 @@ rm(list=ls())
 source('R/import.R')
 source('R/packages.R')
 
+#prepare new files for pre-import
+NewStudies  <-  getStudyNames()
+newS        <-  NewStudies[59]
+makeDataImport(newS)
+setUpFiles(newS)
+
+#process all studies
 d <- loadStudies(reprocess=TRUE)
