ec6c084bccb428e4c2302ac5129b9c66d798d90e
Remko Duursma <remkoduursma@gmail.com>
2013-05-23 10:41:16 +1000

    Removed four bad leaf dry mass data points from Wang2011 (see manipulate)

--- data/Wang2011/dataManipulate.r
+++ data/Wang2011/dataManipulate.r
@@ -1,5 +1,8 @@
 manipulate <- function(raw) {
   
+  # Data for Age=1 clearly unusable: all leaf mass is exactly 0.1g
+  i <- match("Total leaf dry mass (g)", names(raw))
+  raw <- raw[raw[,i] > 0.01,]
   
   raw
 }
