1ecce75e8ef0a7f59f8bf806679ed0ac548dd516
Remko Duursma <remkoduursma@gmail.com>
2013-05-23 11:04:46 +1000

    Added pft to many conifer datasets. grouping in Roth2007

--- data/Brown1978/dataManipulate.R
+++ data/Brown1978/dataManipulate.R
@@ -1,5 +1,11 @@
 manipulate <- function(raw) {
   names(raw)[names(raw)=="species"]  <-  "wrong.sp"
+  
+  # pft, all ae
+  raw$pft <- "EG"
+  raw$pft[raw$species == "LAOC"] <- "DG"
+  
+  
   raw
 }
 
--- data/Roth2007/dataManipulate.R
+++ data/Roth2007/dataManipulate.R
@@ -1,5 +1,8 @@
 manipulate <- function(raw) {
   names(raw)[names(raw)=="species"]  <-  "wrong.sp"
+  
+  raw$grouping <- paste("Soiltype=", raw$remark)
+  
   raw
 }
 
