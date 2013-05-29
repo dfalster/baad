93ab5b38ed27f1a64e72dbd421fe328f6280fd9b
dbarneche <diego.barneche@ymail.com>
2013-05-29 14:32:35 +1000

    fixed files corrupted by the change variable functions

--- data/Kenzo2009/dataManipulate.r
+++ data/Kenzo2009/dataManipulate.r
@@ -1,5 +1,5 @@
 manipulate <- function(raw) {
-  raw[["n.lf.per"]]  <-  (raw[["n.lf.per"]]/100) * raw[["ma.ilf.g.m2"]] # this transforms it from percentage (/unit mass) to /unit area --> final unit g/m2
+  raw[["n.lf.per"]]  <-  (raw[["n.lf.per"]]/100) * raw[["lf.ma.g.m2"]] # this transforms it from percentage (/unit mass) to /unit area --> final unit g/m2
   raw[["group"]]  <-  paste(raw[["group"]], raw[["location"]], raw[["contributor"]], sep="; ")
   
   raw
