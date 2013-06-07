5422f855092ea82fad0910b21c31be13ba76558c
Daniel Falster <daniel.falster@mq.edu.au>
2013-06-07 14:17:27 +1000

    Remove age for bond Lamberty, update questions

--- data/Bond-Lamberty2002/dataManipulate.r
+++ data/Bond-Lamberty2002/dataManipulate.r
@@ -1,7 +1,6 @@
 manipulate <- function(raw) {
   
   raw$grouping  <-  makeGroups(raw, c("Site","HarvestYear","Edaphic"))
-  raw$age       <-  raw[["HarvestYear"]] - raw[["BurnYear"]] 
   raw[["stemMass"]]  <-  raw[["TotBranch"]] + raw[["Stem"]]
   
   raw
