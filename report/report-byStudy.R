8f032a6016a0b9b4687f3c780cf9f09c086fb93a
Daniel Falster <daniel.falster@mq.edu.au>
2013-06-07 14:44:15 +1000

    First round of emails sent to coauthors

--- report/report-byStudy.R
+++ report/report-byStudy.R
@@ -7,19 +7,16 @@ dat <- loadStudies(reprocess=TRUE)
 
 # single study
 printStudyReport(dat, "Aiba2007", reprocess=FALSE)
-printStudyReport(dat, "Lusk2011", reprocess=FALSE)
-
 printStudyReport(dat, "Ishihara0000", reprocess=TRUE)
-emailReport(dat, "Gargaglione2010")
 
 # All reports:
 reportPaths <- printAllStudyReports(dat)
 
-
 # Reprocess one study
-tmp <- loadStudy("Gargaglione2010", reprocess= TRUE)
+tmp <- loadStudy("Selaya2007", reprocess= TRUE)
 dat <- loadStudies(reprocess=FALSE)
 
-
-data = extractStudy(dat, "Bond-Lamberty2002")$data
-
+#Email report
+emailReport(dat, "Gargaglione2010")
+lapply(getStudyNames() ,emailReport, alldata=dat)
+emailReport(dat, "Selaya2008")
