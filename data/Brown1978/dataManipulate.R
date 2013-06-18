2a7facce62072c0d50516e6d5b52f9a5de6021df
Remko Duursma <remkoduursma@gmail.com>
2013-06-18 15:13:22 +1000

    Brown1978 - Added status variable from original dataset, updated location, lat/long, growingCondition, family names, fixed reference, removed a_cs (was calculated from cw and crown length), Added all metadata fields.

--- data/Brown1978/dataManipulate.R
+++ data/Brown1978/dataManipulate.R
@@ -1,11 +1,6 @@
 manipulate <- function(raw) {
   names(raw)[names(raw)=="species"]  <-  "wrong.sp"
   
-  # pft, all ae
-  raw$pft <- "EG"
-  raw$pft[raw$species == "LAOC"] <- "DG"
-  
-  
   raw
 }
 
