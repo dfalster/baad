74582618e77d58ad95903242d1b28be790996e42
Remko Duursma <remkoduursma@gmail.com>
2013-06-19 12:34:42 +1000

    O'Hara1995 - Replaced data.csv with old original source file, now includes status. DBH was in inches (fixed). All other issues fixed as well.

--- data/O'Hara1995/dataManipulate.r
+++ data/O'Hara1995/dataManipulate.r
@@ -1,6 +1,13 @@
 manipulate <- function(raw) {
   
-  raw$grouping <- makeGroups(raw, "site")
+  raw$grouping <- makeGroups(raw, c("habitat","strata"))
+  
+  raw$crownlength <- raw[,"height"] - raw[,"hcb"]
+  
+  raw$sapcb <- (raw$sapcb + raw$sapcb2)/2
+  
+  # Evidently DBH is in inches in the original file
+  raw$dbh <- raw$dbh * 2.54
   
   raw
 }
