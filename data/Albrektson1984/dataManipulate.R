39296b1022493236de5958e5623304bffe2fd6a7
Remko Duursma <remkoduursma@gmail.com>
2013-06-19 13:30:18 +1000

    Albrektson1984 - deleted a.cs

--- data/Albrektson1984/dataManipulate.R
+++ data/Albrektson1984/dataManipulate.R
@@ -3,6 +3,9 @@ manipulate <- function(raw) {
   
   raw$grouping <- makeGroups(raw, "fertilized")
   
+  # remove crown surface area, was calculated from crown width and length
+  raw$a.cs <- NULL
+  
   raw
 }
 
