49721a9556f1370da337a03e2c73d56fb4e3dbf3
Remko Duursma <remkoduursma@gmail.com>
2013-06-19 11:25:09 +1000

    Nouvellon2010 - fixed status variable (max value is now 3 as it should be).

--- data/Nouvellon2010/dataManipulate.r
+++ data/Nouvellon2010/dataManipulate.r
@@ -1,6 +1,21 @@
 manipulate <- function(raw) {
   
   
+  raw$grouping <- makeGroups(raw, "contributor")
+  
+  # Relevel the 'status' variable, which is now coded like this:
+  # Numbered from smaller to larger, corresponding to size class of the inventory
+  # (where 0 = suppressed).See notes in original data.csv.
+  # Here , relevel them so that 0=0, and max(status)=3, with rest in between based on cutting it in bins.
+  # This is done separately for each collection date, because the original data did that as well.
+  # (RAD)
+  stat <- do.call(c,lapply(split(raw, raw$contributor), 
+                           function(x)as.character(cut(x$status/max(x$status),
+                                                       seq(0,1,length=5),
+                                                       include.lowest=TRUE,
+                                                       labels=as.character(0:3)))))
+  raw$status <- unname(stat)
+  
   raw
 }
 
