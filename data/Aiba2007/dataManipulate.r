cce698c0ce7e061b80eac787b24dc871aedd4a88
Remko Duursma <remkoduursma@gmail.com>
2013-06-19 10:53:18 +1000

    Aiba2007 - fixed h.c (from Crown height)

--- data/Aiba2007/dataManipulate.r
+++ data/Aiba2007/dataManipulate.r
@@ -1,5 +1,11 @@
 manipulate <- function(raw) {
-  raw[["species"]]              <-  paste(raw[["genus"]], raw[["species"]])
+  raw[["species"]]  <-  paste(raw[["genus"]], raw[["species"]])
+  
+  raw$crownlength <- raw[,"height (cm)"] - raw[,"crown height (cm)"]
+  
+  # this gives one negative value, set to NA
+  raw$crownlength[raw$crownlength < 0] <- NA
+  
   raw
 }
 
