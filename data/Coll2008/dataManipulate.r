a0e1712e36b2dbd364bf2f1f27d994bdf112a0dd
Remko Duursma <remkoduursma@gmail.com>
2013-06-26 10:27:22 +1000

    Coll2008 - cleaned up dataManipulate.R (ended up with names for a.ilf and m.ilf)

--- data/Coll2008/dataManipulate.r
+++ data/Coll2008/dataManipulate.r
@@ -2,27 +2,21 @@ manipulate <- function(raw) {
   # leaf and biomass measurements come from different individuals, so I'll take 
   # the average leaf measure from each variable and species and then paste this 
   # into the individual trees that contain the biomass data
+  
+  # species level data (leaf vars)
   raw1       <-  raw[!is.na(raw[["tree"]]), 1:6]
-  raw1.mean  <-  
-    data.frame(individual.leaf.mass.g=
-                 tapply(raw1[["individual.leaf.mass.g"]], raw1[["species"]], mean),            
-               individual.leaf.area.cm2=
-                 tapply(raw1[["individual.leaf.area.cm2"]], raw1[["species"]], mean),
-               specific.leaf.area.cm2.g=
-                 tapply(raw1[["specific.leaf.area.cm2/g"]], raw1[["species"]], mean))
+  raw1.mean  <-  aggregate(raw1[,c("individual.leaf.mass.g","individual.leaf.area.cm2","specific.leaf.area.cm2/g")],
+                           by=list(raw1$species),
+                           FUN=mean)
+  names(raw1.mean) <- c("species","individual.leaf.mass.g","individual.leaf.area.cm2","specific.leaf.area.cm2/g")
+    
+  # individual level data (biomass vars)
+  raw       <-  raw[!is.na(raw[["biomass.samples"]]), c(1,7:16)]
   
-  raw       <-  raw[!is.na(raw[["biomass.samples"]]), c(1,4:16)]
-  
-  raw[["individual.leaf.mass.g"]] <- raw1.mean[match(raw[["species"]], rownames(raw1.mean)),
-                                                    "individual.leaf.mass.g"]
-  raw[["individual.leaf.area.cm2"]] <- raw1.mean[match(raw[["species"]], rownames(raw1.mean)), 
-                                                  "individual.leaf.area.cm2"]
-  raw[["specific.leaf.area.cm2/g"]] <- raw1.mean[match(raw[["species"]], rownames(raw1.mean)), 
-                                                    "specific.leaf.area.cm2.g"]
+  # merge
+  raw <- merge(raw, raw1.mean, by="species")
+
   raw[["stem.biomass.g"]] <- raw[["branch.biomass.g"]] + raw[["trunk.biomass.g"]]
-  rm(raw1)
-  
-  raw                            
   
   raw
 }
