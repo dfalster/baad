91afac9eb419382b27110d6f94e069b706561ca4
Remko Duursma <remkoduursma@gmail.com>
2013-06-26 12:57:39 +1000

    Roth2007 - fixed h.c definition

--- data/Roth2007/dataManipulate.R
+++ data/Roth2007/dataManipulate.R
@@ -3,6 +3,10 @@ manipulate <- function(raw) {
   
   raw$grouping <- paste("Soiltype=", raw$remark)
   
+  # h_c was crown length, should be height to crown base
+  raw$h_c <- with(raw, h_t - h_c)
+  
+  
   raw
 }
 
