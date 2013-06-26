cad3c7bc7c39fc62e7e4b3895cd5e52400c620d7
Remko Duursma <remkoduursma@gmail.com>
2013-06-26 11:55:07 +1000

    Fixed wrong h.c in Aiba2007 and OHara1995 (there may be more...)

--- data/Aiba2007/dataManipulate.r
+++ data/Aiba2007/dataManipulate.r
@@ -1,11 +1,6 @@
 manipulate <- function(raw) {
   raw[["species"]]  <-  paste(raw[["genus"]], raw[["species"]])
-  
-  raw$crownlength <- raw[,"height (cm)"] - raw[,"crown height (cm)"]
-  
-  # this gives one negative value, set to NA
-  raw$crownlength[raw$crownlength < 0] <- NA
-  
+    
   raw
 }
 
--- data/O'Hara1995/dataManipulate.r
+++ data/O'Hara1995/dataManipulate.r
@@ -2,8 +2,6 @@ manipulate <- function(raw) {
   
   raw$grouping <- makeGroups(raw, c("habitat","strata"))
   
-  raw$crownlength <- raw[,"height"] - raw[,"hcb"]
-  
   raw$sapcb <- (raw$sapcb + raw$sapcb2)/2
   
   # Evidently DBH is in inches in the original file
