9860c8fff9416e3c7d3d6a68e8f08667ad40bc4e
Remko Duursma <remkoduursma@gmail.com>
2013-06-28 10:02:58 +1000

    Bond-Lamberty2002 - completely redone from original data file. Same problems persist :(

--- data/Bond-Lamberty2002/dataManipulate.r
+++ data/Bond-Lamberty2002/dataManipulate.r
@@ -1,6 +1,7 @@
 manipulate <- function(raw) {
   
   raw$grouping  <-  makeGroups(raw, c("Site","HarvestYear","Edaphic"))
+  raw$age       <-  raw[["HarvestYear"]] - raw[["BurnYear"]] 
   raw[["stemMass"]]  <-  raw[["TotBranch"]] + raw[["Stem"]]
   
   raw
