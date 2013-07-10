944c3e5c4dc916c36feeb84b28d95a1009fc82a8
Remko Duursma <remkoduursma@gmail.com>
2013-07-10 15:10:07 +1000

    Martin1998 - age=0 set to NA (n=3)

--- data/Martin1998/dataManipulate.r
+++ data/Martin1998/dataManipulate.r
@@ -2,6 +2,9 @@ manipulate <- function(raw) {
   raw[["N.lf"]]      <-  (raw[["N.lf"]]/100) #transforms from percentage to proportion, equivalent to kg.kg
   raw$grouping  <-  makeGroups(raw, c("Filter"))
   
+  # age of 0 seems unlikely :)
+  raw$age[raw$age == 0] <- NA
+  
   raw
 }
 
