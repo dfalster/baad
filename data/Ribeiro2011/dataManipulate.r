3e9c6356eda58ccc039043950823bced55fee965
Remko Duursma <remkoduursma@gmail.com>
2013-07-10 14:49:11 +1000

    Ribeiro2011 - zero leaf mass set to NA. Closes #47

--- data/Ribeiro2011/dataManipulate.r
+++ data/Ribeiro2011/dataManipulate.r
@@ -1,5 +1,10 @@
 manipulate <- function(raw) {
   
+  # 9 values have zero leaf mass. The data are correct, as these are decid. trees
+  # that had no leaves at time of sampling. For our database, set these to missing values.
+  nm <- "Leaf.mass..kg."
+  raw[[nm]][raw[[nm]] == 0] <- NA
+  
   
   raw
 }
