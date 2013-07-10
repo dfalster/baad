4c7e9a720a5b226dab037d448446291afff59fd1
Remko Duursma <remkoduursma@gmail.com>
2013-07-10 14:45:29 +1000

    some tidying of csvCompare

--- tests/csvCompare/csvCompare.R
+++ tests/csvCompare/csvCompare.R
@@ -8,14 +8,17 @@ csvCompare <- function(data1, data2, outfile="datacompare.xlsx", colour="red", .
   r <- require(xlsx)
   if(!r)stop("Install the 'xlsx' package first!")
   
-  if(is.character(data1) && file.exists(data1))
-    data1 <- read.csv(data1, ...)
-  if(is.character(data2) && file.exists(data2))
-    data2 <- read.csv(data2, ...)
-
+  getDat <- function(dat){
+    if(is.character(dat) && file.exists(dat))
+      return(read.csv(dat, ...))
+    else
+      return(dat)
+  }
+  data1 <- getDat(data1)  
+  data2 <- getDat(data2)
+  
   d1 <- dim(data1)
   d2 <- dim(data2)
-  
   
   if(d1[1] != d2[1])
     stop("Number of rows differs")
