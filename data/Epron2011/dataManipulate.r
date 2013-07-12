714e4857aa1479c6a5e81b5d10990bca8ae57e0a
Remko Duursma <remkoduursma@gmail.com>
2013-07-12 15:41:46 +1000

    Epron2011 - (Actually Epron2012!) Completely redone from newly emailed data. Data seem good.

--- data/Epron2011/dataManipulate.r
+++ data/Epron2011/dataManipulate.r
@@ -1,17 +1,13 @@
 manipulate <- function(raw) {
-  raw[["a.sbbh"]] <-  raw[["a.st"]] - raw[["a.st1"]]
-  raw[["group"]]  <-  paste(raw[["Variable"]], raw[["contributor"]], raw[["Nutrition"]], sep="; ")
-  raw[["a.cs"]]  <-  pi*(raw[["a.cp"]]/100)^2
-  raw[["n.lf"]]  <-  raw[["n.lf"]]*raw[["a.lf"]]/raw[["m.lf"]]
-  #Error correction - heights for plants age =0.5 at top of datasets in different units to rest of datasets (cm rather than m)
-  i <-1:8
-  raw[["h.t"]][i] <- raw[["h.t"]][i]/100
-  raw[["h.c"]][i] <- raw[["h.c"]][i]/100
   
-  # Zero values in a.ss and a.sh, set to NA
-  raw$a.ss[raw$a.ss==0] <- NA
-  raw$a.sh[raw$a.sh==0] <- NA
+  # 'Better to remove fine root biomass from the database'
+  raw$m.rf <- NULL
   
+  # location
+  raw$location <- paste(raw$location, raw$location2)
+  
+  
+
   raw
 }
 
