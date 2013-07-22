deb3d4c88b653e18420502a001a5704b9814749c
dbarneche <diego.barneche@ymail.com>
2013-07-22 11:27:24 +1000

    Osunkoya2007 - finished processing, changed datanew, datamanipulate, metadata, matchcolumns, sent final email

--- data/Osunkoya2007/dataManipulate.r
+++ data/Osunkoya2007/dataManipulate.r
@@ -1,4 +1,9 @@
 manipulate <- function(raw) {
+  raw$status <- NA
+  raw$status[which(raw$growth_form=="canopy")] <- 3
+  raw$status[which(raw$growth_form=="subcanopy")] <- 1
+  raw$status[which(raw$growth_form=="understorey")] <- 0
+  
   raw$grouping  <-  makeGroups(raw, c("Tree_no","growth_form"))
   
   raw[raw == "#NULL!"] <- NA
