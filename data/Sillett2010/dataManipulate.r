c7c73b8835de8f38ecd8ff49d3e36708e28223c5
Daniel Falster <daniel.falster@mq.edu.au>
2013-07-23 21:48:12 +1000

    Sillett2010 - finished processing. Changed manipulate, metadata, match columns. Sent final email.

--- data/Sillett2010/dataManipulate.r
+++ data/Sillett2010/dataManipulate.r
@@ -8,20 +8,9 @@ manipulate <- function(raw) {
   #remove numbers from Tree label
   raw[["Tree"]]     <-  unlist(lapply(gsub("s ", "s_", raw[["Tree"]]), function(x){strsplit(x, "_")[[1]][1]}))  
   
-  
   # stem mass - difference between total and leaf
   raw$m.st <- (raw[["Dry mass (Mg)"]]*1000 - raw[["Leaf dry mass (kg)"]])
-  
-  # Wood density - stem mass / volume
-  raw$r.st <- raw$m.st/(raw[["Bark volume (m3)"]]+raw[["Heartwood volume (m3)"]]+raw[["Sapwood volume (m3)"]]+raw[["Decaying wood volume (m3)"]])
-  
-  # Sapwood mass
-  raw$m.ss <- raw[["Sapwood volume (m3)"]] *  raw$r.st 
-  #Heartwood mass  
-  raw$m.sh <-(raw[["Heartwood volume (m3)"]]+raw[["Decaying wood volume (m3)"]]) *  raw$r.st 
-  #Bark mass
-  raw$m.sb <- raw[["Sapwood volume (m3)"]] *  raw$r.st 
-  
+   
   raw
 }
 
