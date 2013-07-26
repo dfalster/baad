d7b6c050380410e8236a997e95bd1ceb2e1d5187
Remko Duursma <remkoduursma@gmail.com>
2013-07-26 16:06:10 +1000

    Roth2007 - completely redone from new raw data.

--- data/Roth2007/dataManipulate.R
+++ data/Roth2007/dataManipulate.R
@@ -1,10 +1,36 @@
 manipulate <- function(raw) {
-  names(raw)[names(raw)=="species"]  <-  "wrong.sp"
+
   
-  raw$grouping <- paste("Soiltype=", raw$remark)
+  # geometric mean crown width
+  raw$CW <- with(raw, sqrt(Cwalong * Cwacross))
   
-  # h_c was crown length, should be height to crown base
-  raw$h_c <- with(raw, h_t - h_c)
+  # crown width in mixed units
+  raw$CW[raw$CW < 10] <- 100*raw$CW[raw$CW < 10]
+  # Species. 
+  raw$speciesfix <- "Pinus taeda"
+  raw$speciesfix[raw$Species2 == "S"] <- "Pinus elliottii"
+  
+  # DensityID contains stand density:
+  raw$standdensity <- raw$DensityID
+  raw$standdensity[raw$standdensity=="N"] <- "2990ha-1"
+  raw$standdensity[raw$standdensity=="W"] <- "1334ha-1"
+  
+  # Silviculturalinputs, recode
+  raw$Silviculturalinputs[raw$Silviculturalinputs == "H"] <- "High"
+  raw$Silviculturalinputs[raw$Silviculturalinputs == "L"] <- "Low"
+  
+  # grouping
+  raw$grouping <- makeGroups(raw, c("soil","Silviculturalinputs","standdensity","FamilyID"))
+  
+  # Coarse root biomass
+  raw$CoarseR20[is.na(raw$CoarseR20)] <- 0
+  raw$CoarseR40[is.na(raw$CoarseR40)] <- 0
+  raw$Taproot[is.na(raw$Taproot)] <- 0
+  raw$CoarseRoot[is.na(raw$CoarseRoot)] <- 0
+  
+  raw$coarseroot <- with(raw, CoarseR20 + CoarseR40 + Taproot + CoarseRoot)
+  raw$coarseroot[raw$coarseroot == 0] <- NA
+  
   
   
   raw
