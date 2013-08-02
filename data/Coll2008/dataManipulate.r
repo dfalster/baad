ddb581cdeb0bd73d7c5813b76aa6ba01bc7c4001
dbarneche <diego.barneche@ymail.com>
2013-08-02 10:08:17 +1000

    Coll2008 - processed data, sent email requesting species pft.

--- data/Coll2008/dataManipulate.r
+++ data/Coll2008/dataManipulate.r
@@ -1,23 +1,6 @@
 manipulate <- function(raw) {
-  # leaf and biomass measurements come from different individuals, so I'll take 
-  # the average leaf measure from each variable and species and then paste this 
-  # into the individual trees that contain the biomass data
-  
-  # species level data (leaf vars)
-  raw1       <-  raw[!is.na(raw[["tree"]]), 1:6]
-  raw1.mean  <-  aggregate(raw1[,c("individual.leaf.mass.g","individual.leaf.area.cm2","specific.leaf.area.cm2/g")],
-                           by=list(raw1$species),
-                           FUN=mean)
-  names(raw1.mean) <- c("species","individual.leaf.mass.g","individual.leaf.area.cm2","specific.leaf.area.cm2/g")
-    
-  # individual level data (biomass vars)
-  raw       <-  raw[!is.na(raw[["biomass.samples"]]), c(1,7:16)]
-  
-  # merge
-  raw <- merge(raw, raw1.mean, by="species")
-
+  raw  <-  raw[is.na(raw$individual.leaf.mass.g),]
   raw[["stem.biomass.g"]] <- raw[["branch.biomass.g"]] + raw[["trunk.biomass.g"]]
-  
   raw
 }
 
