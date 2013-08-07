6e3f5059661ef889ed7d42e008165e75201c290e
dbarneche <diego.barneche@ymail.com>
2013-08-07 12:29:07 +1000

    Ishihara0000 - Implemented initial set up for all 6 subdivisions.

--- R/load.R
+++ R/load.R
@@ -1,5 +1,13 @@
 rm(list=ls())
 source('R/import.R')  
 
+#identify new studies
+newStudies  <-  identifyNewStudies()
+if(length(newStudies) > 0){
+  lapply(newStudies, makeDataImport)
+}
+if(length(newStudies) > 0){
+  lapply(newStudies, setUpFiles, quiet=FALSE)
+}
 #process all studies
 d <- loadStudies(reprocess=TRUE)
