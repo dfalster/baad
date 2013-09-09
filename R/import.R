52a3b978b81d1bef5c27701d33fc2c952cc0a34d
Diego Barneche <barnechedr@gmail.com>
2013-09-09 16:14:24 +1000

    loads and/or install all necessary packages for the project not listed within dataMashR

--- R/import.R
+++ R/import.R
@@ -1,5 +1,15 @@
-library(bibtex, quietly=TRUE)
+checkPackage  <-  function(package.name){
+  if(package.name %in% .packages(all.available=TRUE)){
+    library(package.name, character.only=TRUE, quietly=TRUE)  
+  } else {
+    install.packages(package.name)
+    library(package.name, character.only=TRUE, quietly=TRUE)
+  }
+  
+}
 
+sapply(c('bibtex', 'devtools', 'maps', 'mapdata', 'gdata', 'smatr'), checkPackage)
+rm(checkPackage)
 
 if("dataMashR" %in% .packages(all.available=TRUE)){
   library(dataMashR)  
