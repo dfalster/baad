c3971ad9dbd5c3327db3a5d14a441a63bd68799a
Daniel Falster <daniel.falster@mq.edu.au>
2013-10-28 13:15:09 +1100

    Ishihara0000, Nishioka1982, Yoda1978: remove vines, finish processing, send final report

--- data/Ishihara0000/dataManipulate.R
+++ data/Ishihara0000/dataManipulate.R
@@ -1,5 +1,9 @@
 manipulate  <-  function(raw){
 
-	raw
+	# Remove vine species
+	vines <- c("Trachelospermum asiaticum", "Akebia trifoliata", "Gardneria nutans", "Cocculus trilobus", "Wisteria floribunda", "Vitis saccharifera", "Hedera rhombea", "Ficus nipponica")
 
+	raw <- subset(raw, !(species %in% vines))
+
+	raw
 }
