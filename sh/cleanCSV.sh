714f49a87ba5d8d4e1690585a3e1c2a096fe3571
Daniel Falster <daniel.falster@mq.edu.au>
2013-12-18 15:36:02 +1100

    Add option to script for cleaning line endings

--- sh/cleanCSV.sh
+++ sh/cleanCSV.sh
@@ -1,12 +1,3 @@
-# Renames pdf to stadard name format
-#to run type bash sh/renamePdf.sh ls data/   
-cd data
-for filename in *
-do
-   cd $filename
-   for file in *.csv
-   do
-	perl -pi -e 's/\r/\n/g' $file
-   done
-   cd ..
-done
+
+perl -i -pi -e 's/\r\n/\n/g' $* 	# change windows line ends to unix
+perl -i -pi -e 's/\r/\n/g' $* 		# change oldtsyl mac line endings to unix
