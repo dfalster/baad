2a5a44e83bfa08753dc83cb3201cd96c37a2547c
Remko Duursma <remkoduursma@gmail.com>
2014-01-24 16:12:05 +1100

    Saldana-Acosta2009 - many zeroes removed (rows in dataset with only initial height are useless)

--- data/Saldana-Acosta2009/dataManipulate.R
+++ data/Saldana-Acosta2009/dataManipulate.R
@@ -1,4 +1,8 @@
 manipulate <- function(raw) {
 
+  # A number of rows in this dataset had only initial height, no other vars.
+  # Delete these.
+  raw <- raw[raw[["Final height (cm)"]] > 0,]
+  
   raw
 }
