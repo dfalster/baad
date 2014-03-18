5a8d5a2e68b40f4c52416a10189dc3b3d16ea576
Remko Duursma <remkoduursma@gmail.com>
2014-03-19 10:11:03 +1100

    some analysis of metadata and tables started

---
+++ analysis/metadata_and_tables.R
@@ -0,0 +1,92 @@
+
+dat <- readRDS("cache/allomdata_post.rds")
+
+# Sample size
+nrow(dat)
+
+# nr of species
+length(unique(dat$species))
+
+
+
+# User supplied MAP (mm) and MAT (degC) vs. values obtained from Worldclim.
+pdf(file="analysis/output/figures/MAP_MAT_Worldclim_user.pdf", width=8, height=4)
+with(dat,{
+  
+  par(mfrow=c(1,2))
+  plot(MAP, map, xlab="MAP - WorldClim", ylab="MAP - User supplied")
+  abline(0,1)
+  plot(MAT, mat, xlab="MAT - WorldClim", ylab="MAT - User supplied")
+  abline(0,1)
+  
+})
+dev.off()
+
+
+
+# Family
+sort(xtabs(~ family, data=dat),T)
+
+# Vegetation and plant functional type
+ftable(xtabs(~ vegetation + pft, data=dat))
+
+#
+
+
+# Comparisons to be made:
+#-------------------------------------------------------------------------------------------#
+#- Five major families (5 most common families in this dataset)
+majorfam <- c("Pinaceae","Myrtaceae","Fagaceae","Betulaceae","Dipterocarpaceae")
+dat5fam <- droplevels(subset(dat, family %in% majorfam))
+
+# Family vs. PFT
+ftable(xtabs(~ family + pft, data=dat5fam))
+
+# Family vs. vegetation
+ftable(xtabs(~ family + vegetation, data=dat5fam))
+
+# But note,
+sum(is.na(dat$family)) # 1342
+
+
+
+#-------------------------------------------------------------------------------------------#
+# Tropical vs. temperate evergreen angiosperm:
+# Both are large groups, do they have similar allometry or does climate influence biomass proportions
+# at a given size?
+
+datEA <- droplevels(subset(dat, pft == "EA" & vegetation %in% c("TempF","TempRF","TropRF","TropSF")))
+
+with(datEA, plot(as.factor(vegetation), MAP))
+with(datEA, plot(as.factor(vegetation), MAT))
+
+datEA$TempTrop <- as.factor(ifelse(datEA$vegetation %in% c("TempF","TempRF"), "Temp", "Trop"))
+
+
+# Randomize rows for plotting
+datEA <- datEA[sample(nrow(datEA)),]
+
+palette(c("blue","red"))
+# some quick plots
+pdf("analysis/output/figures/Trop_vs_temp_plots.pdf")
+with(datEA, plot(log10(m.st), log10(m.lf), pch=19,cex=0.8, col=TempTrop))
+legend("topleft", levels(datEA$TempTrop), pch=19, col=palette())
+with(datEA, plot(log10(h.t), log10(m.lf), pch=19,cex=0.8, col=TempTrop))
+with(datEA, plot(log10(d.bh), log10(m.lf), pch=19,cex=0.8, col=TempTrop))
+with(datEA, plot(log10(d.bh), log10(h.t), pch=19,cex=0.8, col=TempTrop))
+with(datEA, plot(log10(d.bh), log10(m.so), pch=19,cex=0.8, col=TempTrop))
+with(datEA, plot(log10(h.t), log10(m.so), pch=19,cex=0.8, col=TempTrop))
+with(datEA, plot(log10(m.so), log10(m.lf/m.so), pch=19,cex=0.8, col=TempTrop))
+with(datEA, plot(log10(m.so), log10(a.lf/m.so), pch=19,cex=0.8, col=TempTrop))
+dev.off()
+
+
+#-------------------------------------------------------------------------------------------#
+
+
+
+
+
+
+
+
