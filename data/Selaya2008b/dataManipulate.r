12fb880656b9f0806bda37bae7c333b95eff2d11
Remko Duursma <remkoduursma@gmail.com>
2014-03-21 14:55:30 +1100

    Selaya2008b - Fixed groupings (for issue #67)

--- data/Selaya2008b/dataManipulate.r
+++ data/Selaya2008b/dataManipulate.r
@@ -2,6 +2,9 @@ manipulate <- function(raw) {
   raw        <-  raw[raw[["reference"]]=="Selaya et al. (2008) Journal of Ecology 96: 1211-1221; Selaya & Anten (2010) Ecology 91:1102-1113",]
   raw[["light"]]  <-  raw[["light"]]/55*100
 
+  # Fix grouping. This info is also in 'age', but repeat it here to be safe.
+  names(raw)[names(raw) == "group..7."] <- "grouping"
+  raw$grouping <- makeGroups(raw, "grouping")
   
   raw
 }
