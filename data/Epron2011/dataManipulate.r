da5423adc9d05652a5aa41a4dbca7d85f7cd3af0
Remko Duursma <remkoduursma@gmail.com>
2014-03-21 14:53:48 +1100

    Epron2011 - Fixed groupings (for issue #67)

--- data/Epron2011/dataManipulate.r
+++ data/Epron2011/dataManipulate.r
@@ -6,7 +6,9 @@ manipulate <- function(raw) {
   # location
   raw$location <- paste(raw$location, raw$location2)
   
-  
+  # grouping variable has fertilization in it, extract
+  raw$fertilization <- gsub("Destructive, Laclau, ","", raw$grouping)
+  raw$grouping <- makeGroups(raw, "fertilization")
 
   raw
 }
