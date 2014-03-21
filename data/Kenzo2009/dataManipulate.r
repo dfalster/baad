33b99fc56c0fc90f15674cfbeb6653eb64c2c961
Remko Duursma <remkoduursma@gmail.com>
2014-03-21 14:54:18 +1100

    Kenzo2009 - Fixed groupings (for issue #67)

--- data/Kenzo2009/dataManipulate.r
+++ data/Kenzo2009/dataManipulate.r
@@ -1,6 +1,7 @@
 manipulate <- function(raw) {
   raw[["n.lf.per"]]  <-  raw[["n.lf.per"]]/100 # this transforms it from percentage to proportion --> equivalent to kg/kg
-  raw[["group"]]  <-  paste(raw[["group"]], raw[["location"]], raw[["contributor"]], sep="; ")
+  
+  raw$grouping <- makeGroups(raw, c("location","contributor","group"))
     
   raw
 }
