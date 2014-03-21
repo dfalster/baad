e0026290f6b120123c8ce8a54bd84e415af0bc9c
Remko Duursma <remkoduursma@gmail.com>
2014-03-21 14:55:00 +1100

    Rodriguez2003 - Fixed groupings (for issue #67)

--- data/Rodriguez2003/dataManipulate.r
+++ data/Rodriguez2003/dataManipulate.r
@@ -1,5 +1,10 @@
 manipulate <- function(raw) {
   raw[["Bark_Stemwood"]]                    <-  raw[["Bark"]] + raw[["Stemwood"]]
+  
+  # To be consistent with publication Table 1.
+  raw$Treatment <- paste0("T", raw$Treatment)
+  raw$grouping <- makeGroups(raw, "Treatment")
+  
   raw
 }
 
