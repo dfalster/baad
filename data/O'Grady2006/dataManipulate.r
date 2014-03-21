eb9fd86e7b04f8286407fdf1200ad8c3d9146680
Remko Duursma <remkoduursma@gmail.com>
2014-03-21 14:54:39 +1100

    O'Grady2006 - Fixed groupings (for issue #67)

--- data/O'Grady2006/dataManipulate.r
+++ data/O'Grady2006/dataManipulate.r
@@ -3,7 +3,7 @@ manipulate <- function(raw) {
   raw[["stem"]]       <-  raw[["stem"]]+raw[["branch"]]
   raw[["m.rf"]]       <-  raw[["5-Oct"]] + raw[["Oct-15"]]
   raw[["m.rc"]]       <-  raw[["15 - 20"]] + raw[["20  + stump"]]
-  raw$grouping        <-  makeGroups(raw, c("Harvested","plot","treatment","seedling"))
+  raw$grouping        <-  makeGroups(raw, c("Harvested","plot"))
   
   #Exclude data from zulu plot, these seem to be in different units. Send query to author
   raw  <-  raw[raw[["plot"]] != "zulu",]
