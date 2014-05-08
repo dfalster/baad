4b74ba5856f9bb51dc549cd4ca97ea6c697e021c
dbarneche <diego.barneche@ymail.com>
2014-05-09 09:02:54 +1000

    removed file that gets author contributions; functions being sourced only from within ms.Rmd now

--- ms/authorsContributions.R
+++
@@ -1,9 +0,0 @@
-source('ms/MS-fun.R')
-source('R/build-baad-fun.R')
-library(plyr)
-library(dataMashR)
-
-data.path      <-  dataMashR::data.path
-data           <-  loadData(reprocess=TRUE)$data
-wanted         <-  contributionColumns()
-contributions  <-  getContributions(data, wanted)
