c15ac2216b54ffbb3821a784524748ade9dd7e9e
Remko Duursma <remkoduursma@gmail.com>
2014-05-13 09:30:09 +1000

    missing pft for Ando1973. For issue #63

--- data/Ando1973/dataManipulate.R
+++ data/Ando1973/dataManipulate.R
@@ -1,7 +1,8 @@
 manipulate  <-  function(raw){
 
 	# Remove vine species
-	vines <- c("Trachelospermum asiaticum", "Akebia trifoliata", "Gardneria nutans", "Cocculus trilobus", "Wisteria floribunda", "Vitis saccharifera", "Hedera rhombea", "Ficus nipponica")
+	vines <- c("Trachelospermum asiaticum", "Akebia trifoliata", "Gardneria nutans", 
+             "Cocculus trilobus", "Wisteria floribunda", "Vitis saccharifera", "Hedera rhombea", "Ficus nipponica")
 
 	raw <- subset(raw, !(species %in% vines))
 
