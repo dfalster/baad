64954bf291fe2f7bd59898c5aa8dff15322094be
Daniel Falster <daniel.falster@mq.edu.au>
2014-05-13 13:50:02 +1000

    O'Grady 2006, 2000: review

--- data/O'Grady2006/dataManipulate.r
+++ data/O'Grady2006/dataManipulate.r
@@ -1,11 +1,11 @@
 manipulate <- function(raw) {
-  
+
   raw[["stem"]]       <-  raw[["stem"]]+raw[["branch"]]
   raw[["m.rf"]]       <-  raw[["5-Oct"]] + raw[["Oct-15"]]
   raw[["m.rc"]]       <-  raw[["15 - 20"]] + raw[["20  + stump"]]
   raw$grouping        <-  makeGroups(raw, c("Harvested","plot"))
-  
-  #Exclude data from zulu plot, these seem to be in different units. Send query to author
+
+  #Exclude data from zulu plot, these seem to be in different units
   raw  <-  raw[raw[["plot"]] != "zulu",]
   raw  <-  raw[!(raw[["plot"]] == 1 & raw[["seedling"]] ==16),]
   raw
