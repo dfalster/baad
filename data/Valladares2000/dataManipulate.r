e72ac213332491a5eeb5ccb2e31c47cd4749cbfb
Remko Duursma <remkoduursma@gmail.com>
2014-05-15 12:13:35 +1000

    Valladares2000 - species name fix

--- data/Valladares2000/dataManipulate.r
+++ data/Valladares2000/dataManipulate.r
@@ -1,7 +1,7 @@
 manipulate <- function(raw) {
   raw[["species"]]  <-  gsub("P.", "Psychotria", raw[["species"]])
   
-  
+    
   # h.c was crown length, should be height to crown base
   raw$h.c <- with(raw, h.t - h.c)
   raw$h.c[raw$h.c < 0] <- 0
