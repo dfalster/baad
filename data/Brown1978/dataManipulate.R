34cd525e01c51586c261ba560f38b1f21f3a685d
Remko Duursma <remkoduursma@gmail.com>
2014-05-16 10:52:56 +1000

    Brown1978 - zero fix in datamanipulate

--- data/Brown1978/dataManipulate.R
+++ data/Brown1978/dataManipulate.R
@@ -1,12 +1,14 @@
 manipulate <- function(raw) {
   names(raw)[names(raw)=="species"]  <-  "wrong.sp"
   
-  
   # h_c is crown length; should be height to crownbase
   raw$h_c <- with(raw, h_t - h_c)
   
   # zero d_bh is NA (note: zero dbh is possible when h.c < 1.4 or 1.3).
   raw$dbh[raw$dbh == 0] <- NA
+  
+  # zero a.stbh fix
+  raw$a_st[raw$a_st == 0] <- NA
   
   raw
 }
