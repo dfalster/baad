26fd3e50c0d13d7f550791f4fba3ae81f14352e5
Remko Duursma <remkoduursma@gmail.com>
2014-05-16 11:46:11 +1000

    Poorter1999 - set zero c.d to NA, also bug fix in c.dr calc

--- data/Poorter1999/dataManipulate.R
+++ data/Poorter1999/dataManipulate.R
@@ -1,4 +1,8 @@
 manipulate <- function(raw) {
-  raw[["d.cr"]] <- 0.5*(raw[["d.cr"]]+raw[["d.cr"]])
+  
+  raw[["d.cr"]] <- 0.5*(raw[["d.cr"]]+raw[["d.cr2"]])
+  
+  raw$c.d[raw$c.d == 0] <- NA
+  
   raw
 }
