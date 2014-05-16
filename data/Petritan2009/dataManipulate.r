d3c247bf3e1a5dd39da3318b70a62f2f3ac51858
Remko Duursma <remkoduursma@gmail.com>
2014-05-16 11:34:21 +1000

    Petritan2009 - fix zero crown length

--- data/Petritan2009/dataManipulate.r
+++ data/Petritan2009/dataManipulate.r
@@ -1,5 +1,9 @@
 manipulate <- function(raw) {
   
+  # fix zero crown length (make NA)
+  i <- raw[["Crown.lenght..cm."]]==0
+  raw[["Height.to.crown.base..cm."]][i] <- NA
+  raw[["Crown.lenght..cm."]][i] <- NA
   
   raw
 }
