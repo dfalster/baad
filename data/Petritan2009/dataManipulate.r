f0709f565f66791e689ffbe1c63ffeb38820628c
Remko Duursma <remkoduursma@gmail.com>
2014-05-16 11:37:40 +1000

    Petritan2009 - fixed again

--- data/Petritan2009/dataManipulate.r
+++ data/Petritan2009/dataManipulate.r
@@ -1,7 +1,7 @@
 manipulate <- function(raw) {
   
   # fix zero crown length (make NA)
-  i <- raw[["Crown.lenght..cm."]]==0
+  i <- raw[["Total.height.of.plant..cm."]] - raw[["Height.to.crown.base..cm."]] ==0
   raw[["Height.to.crown.base..cm."]][i] <- NA
   raw[["Crown.lenght..cm."]][i] <- NA
   
