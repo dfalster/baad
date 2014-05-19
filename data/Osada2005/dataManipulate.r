a9bdb52927969689863a167dc5cb0c543d0c32c3
Remko Duursma <remkoduursma@gmail.com>
2014-05-19 10:35:14 +1000

    Osada2005 - removed unnecessary ma.ilf calcs in datamanipulate (fixes zeroes)

--- data/Osada2005/dataManipulate.r
+++ data/Osada2005/dataManipulate.r
@@ -1,7 +1,6 @@
 manipulate <- function(raw) {
   raw           <-  raw[raw[["Source"]] == "Osada (2005) Canadian Journal of Botany", ]
-  raw[["SLA"]]  <-  (1/raw[["SLA"]])*10000
-  raw[["LMA"]]  <-  mapply(function(x,y){sum(c(x,y),na.rm=TRUE)},raw[["SLA"]],raw[["LMA"]])  
+    
   raw
 }
 
