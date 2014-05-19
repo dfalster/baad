e3bca0017df6f636cfadee396c77a744018620a2
Remko Duursma <remkoduursma@gmail.com>
2014-05-19 10:42:51 +1000

    Martin1998 - fix zero n.ba, n.ss, n.sh

--- data/Martin1998/dataManipulate.r
+++ data/Martin1998/dataManipulate.r
@@ -5,6 +5,11 @@ manipulate <- function(raw) {
   # age of 0 seems unlikely :)
   raw$age[raw$age == 0] <- NA
   
+  # Zero N% fix
+  raw$N.sa[raw$N.sa == 0] <- NA
+  raw$N.ht[raw$N.ht == 0] <- NA
+  raw$N.ba[raw$N.ba == 0] <- NA
+  
   raw
 }
 
