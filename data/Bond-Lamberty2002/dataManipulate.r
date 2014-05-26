ed85c4f82f0a7ec6173c18c802d37c388105d3ca
Remko Duursma <remkoduursma@gmail.com>
2014-05-26 10:24:36 +1000

    Bond-Lamberty2002 - d.bh in 3 observations set to NA because h.t < 1.37m where dbh was reportedly measured. d.ba is available instead. For issue #105

--- data/Bond-Lamberty2002/dataManipulate.r
+++ data/Bond-Lamberty2002/dataManipulate.r
@@ -4,6 +4,10 @@ manipulate <- function(raw) {
   raw$age       <-  raw[["HarvestYear"]] - raw[["BurnYear"]] 
   raw[["stemMass"]]  <-  raw[["TotBranch"]] + raw[["Stem"]]
   
+  # three observations are h.t < 1.37, and d.bh is recorded (even though dbh is measured at 1.37 in this case)
+  # set to NA
+  raw$DBH[raw$Height < 137] <- NA
+  
   # zero root mass
   raw$Root[raw$Root == 0] <- NA
   
