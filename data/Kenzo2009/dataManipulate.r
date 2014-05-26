8fbfe16c97351ba09efb9de823dab142b873960e
Remko Duursma <remkoduursma@gmail.com>
2014-05-26 10:29:51 +1000

    Kenzo2009 - 3 observations with h.t<1.3m had d.bh recorded; set to NA (d.ba available instead). For issue #105

--- data/Kenzo2009/dataManipulate.r
+++ data/Kenzo2009/dataManipulate.r
@@ -2,7 +2,11 @@ manipulate <- function(raw) {
   raw[["n.lf.per"]]  <-  raw[["n.lf.per"]]/100 # this transforms it from percentage to proportion --> equivalent to kg/kg
   
   raw$grouping <- makeGroups(raw, c("location","contributor","group"))
-    
+  
+  
+  # Three observations are <1.3m but have d.bh recorded. Set to NA
+  raw$a.stbh.cm[raw$h.t.m <= 1.3] <- NA
+  
   raw
 }
 
