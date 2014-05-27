222490f0f3e55a55c17580256c82595d613b8b00
Remko Duursma <remkoduursma@gmail.com>
2014-05-27 13:43:20 +1000

    Osada2005 - added h.bh as 10% of h.t; and added d.bh (it was not previously) For issue #105

--- data/Osada2005/dataManipulate.r
+++ data/Osada2005/dataManipulate.r
@@ -1,6 +1,9 @@
 manipulate <- function(raw) {
   raw           <-  raw[raw[["Source"]] == "Osada (2005) Canadian Journal of Botany", ]
-    
+
+  # Height of diameter measurement at 10% of tree height (see metadata)
+  raw$h.bh <- 0.1 * raw$Height
+  
   raw
 }
 
