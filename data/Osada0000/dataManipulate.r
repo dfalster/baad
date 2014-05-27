5116c3075eefd315eeb2c249f299b601b7cc73ea
Remko Duursma <remkoduursma@gmail.com>
2014-05-27 13:35:23 +1000

    Osada0000 - set h.bh to 10% of tree height (for issue #105), also fixed SLA calcs.

--- data/Osada0000/dataManipulate.r
+++ data/Osada0000/dataManipulate.r
@@ -1,7 +1,8 @@
 manipulate <- function(raw) {
   raw           <-  raw[raw[["Source"]] != "Osada et al. (2003) Forest Ecology and Management" & raw[["Source"]] != "Osada (2005) Canadian Journal of Botany", ]
-  raw[["SLA"]]  <-  (1/raw[["SLA"]])*10000
-  raw[["LMA"]]  <-  mapply(function(x,y){sum(c(x,y),na.rm=TRUE)},raw[["SLA"]],raw[["LMA"]]) 
+  
+  # Height of diameter measurement at 10% of tree height (see metadata)
+  raw$h.bh <- 0.1 * raw$Height
   raw
 }
 
