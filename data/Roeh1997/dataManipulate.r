8999f30a371e733f862fc89870145b96b2c35a37
Remko Duursma <remkoduursma@gmail.com>
2014-06-17 13:42:36 +1000

    Roeh1997 - data.csv updated with stand information, location, lat/long where available. Metadata filled in where known.

--- data/Roeh1997/dataManipulate.r
+++ data/Roeh1997/dataManipulate.r
@@ -1,7 +1,6 @@
 manipulate <- function(raw) {
   
-  # h_c was crownlength, should be height to crown base
-  raw$h.c <- with(raw, h.t - h.c)
+  raw$grouping <- makeGroups(raw, c("Installation","Plot"))
   
   
   raw
