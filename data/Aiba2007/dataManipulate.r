59c914273b58f99e6c52137f235dbe1cd780d107
Remko Duursma <remkoduursma@gmail.com>
2014-06-20 11:06:11 +1000

    Aiba2007 - set h.c>h.t to NA

--- data/Aiba2007/dataManipulate.r
+++ data/Aiba2007/dataManipulate.r
@@ -1,6 +1,11 @@
 manipulate <- function(raw) {
   raw[["species"]]  <-  paste(raw[["genus"]], raw[["species"]])
-    
+  
+  
+  # Set height to crown base > plant height to NA
+  i <- raw[["crown height (cm)"]] > raw[["height (cm)"]]
+  rawraw[["crown height (cm)"]][i] <- NA
+
   raw
 }
 
