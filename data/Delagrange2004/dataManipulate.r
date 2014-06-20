9f99359d6cb76e0bdd94558917d0cdccaa2b2866
Remko Duursma <remkoduursma@gmail.com>
2014-06-20 11:07:27 +1000

    Delagrange2004 - set h.c>h.t to NA

--- data/Delagrange2004/dataManipulate.r
+++ data/Delagrange2004/dataManipulate.r
@@ -4,6 +4,9 @@ manipulate <- function(raw) {
   
   raw$n.lf <-  raw$n.lf*raw$LeafArea*0.0001/raw$m.lf
   
+  # Height to crown base cannot be > h.t; set to NA
+  raw$h.c[raw$h.c > raw$h.t] <- NA
+  
   raw
 }
 
