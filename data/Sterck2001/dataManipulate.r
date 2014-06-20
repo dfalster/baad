a2dc0f73617162da92c320217dc04c843955911e
Remko Duursma <remkoduursma@gmail.com>
2014-06-20 10:54:50 +1000

    Sterck2001 - crown width geometric mean of two measurements; height to crown base > total plant height set to NA

--- data/Sterck2001/dataManipulate.r
+++ data/Sterck2001/dataManipulate.r
@@ -4,6 +4,12 @@ manipulate <- function(raw) {
   # Obvious typo
   raw[["Ht2"]][raw[["Ht2"]] > 50] <- NA
   
+  # When height to crown base higher than total plant height
+  raw$Ht2[raw$Ht2 > raw$Ht] <- NA
+  
+  # Geometric mean of two crown widths
+  raw$CW <- sqrt(raw$CW1 * raw$CW2)
+  
   raw
 }
 
