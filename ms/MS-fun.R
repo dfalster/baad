5a68acf80cb1cca7a7c6e2c15ab4453ce9a5d4ab
Daniel Falster <daniel.falster@mq.edu.au>
2014-06-25 12:38:35 +1000

    Refine paper

--- ms/MS-fun.R
+++ ms/MS-fun.R
@@ -1,3 +1,8 @@
+library(knitr, quietly = TRUE)
+library(maps, quietly = TRUE)
+library(bibtex, quietly = TRUE)
+library(knitcitations, quietly = TRUE)
+
 ## Mimic dataMashR function here, but relative to *this* directory.
 data.path <- function(...) {
   file.path("../data", ...)
@@ -137,3 +142,99 @@ listOfAuthors  <-  function(data) {
 }
 
 
+capitalize <- function (string) {
+    capped <- grep("^[^A-Z]*$", string, perl = TRUE)
+    substr(string[capped], 1, 1) <- toupper(substr(string[capped],
+        1, 1))
+    string
+}
+
+## Make colours semitransparent:
+make.transparent <- function(col, opacity = 0.5) {
+    if (length(opacity) > 1 && any(is.na(opacity))) {
+        n <- max(length(col), length(opacity))
+        opacity <- rep(opacity, length.out = n)
+        col <- rep(col, length.out = n)
+        ok <- !is.na(opacity)
+        ret <- rep(NA, length(col))
+        ret[ok] <- Recall(col[ok], opacity[ok])
+        ret
+    } else {
+        tmp <- col2rgb(col)/255
+        rgb(tmp[1, ], tmp[2, ], tmp[3, ], alpha = opacity)
+    }
+}
+
+drawWorldPlot  <-  function(data, sizebyn=FALSE, add=FALSE,
+                            pchcol="red",
+                            legend=TRUE){
+
+
+  require(maps)
+  if(!add){
+    map('world',col="grey80",bg="white",lwd=0.5,fill=TRUE,resolution=0,wrap=TRUE, border="grey80", ylim=c(-80, 80), mar=rep(0,4))
+    map('world',col="black",boundary=TRUE,lwd=0.2,interior=FALSE,fill=FALSE,add=TRUE,resolution=0,wrap=TRUE)
+  }
+
+
+  # Remove all duplicates (increases speed and minimizes file size)
+  latlon <- with(data, paste(latitude, longitude))
+  lat <- data$latitude[!duplicated(latlon)]
+  lon <- data$longitude[!duplicated(latlon)]
+
+  j  <-  !is.na(lat) & !is.na(lon)
+
+  # Location only sometimes missing - but lat/lon can still be in dataset anyway.
+  # & data$loc != "NA" | is.na(data$loc)
+
+  if(!any(j)){
+    polygon(c(-100,95,95,-100), c(-10,-10,15,15), col=rgb(0,0,0,240,maxColorValue=255))
+    text(-100, 0, expression(paste(bold("Missing coordinate/location"))), col="red", xpd=TRUE, pos=4, cex=0.8)
+  } else {
+
+    if(!sizebyn){
+      points(lon,lat, pch=19, col=make.transparent("blue",0.5), cex=1.6)
+    } else {
+      n <- table(latlon)
+      symbols(lon,lat, circles=log10(n), inches=0.1, fg="black", bg=pchcol, add=TRUE)
+
+      if(legend){
+        ns <- c(10,100,1000)
+        X <- rep(-170,3)
+        Y <- seq(-30,-10,by=10)
+        rect(xleft=-200, xright=-120, ybottom=-50, ytop=10, col="white", border=NA)
+        symbols(x=X, y=Y, circles=log10(ns), inches=0.1,
+                fg="black", bg=pchcol, add=TRUE)
+        text(x=X+5, y=Y, labels=as.character(ns),pos=4)
+        text(x=X+5, y=Y[3]+10, labels=expression(italic(n)), pos=4)
+      }
+    }
+  }
+}
+
+
+summaryTable <- function(data, var.def, digits = 2){
+
+	thesevars <- setdiff(var.def$Variable[var.def$Type=="numeric"], c("map","mat","lai"))
+
+	N <- sapply(thesevars, function(x)length(data[[x]][!is.na(data[[x]])]))
+
+	Nstud <- sapply(thesevars, function(x)length(unique(data[["studyName"]][!is.na(data[[x]])])))
+
+	df <- rbind.fill(apply(data[,thesevars], 2, function(x)data.frame(Min=min(x, na.rm=TRUE),Max=max(x, na.rm=TRUE),Median=median(x, na.rm=TRUE))))
+	dfr <- data.frame(Variable=thesevars, N=N, Studies=Nstud)
+
+	dfr <- cbind(dfr,df)
+
+	for(v in c("Min", "Max", "Median"))
+		dfr[[v]] <- formatC(dfr[[v]], digits=digits, format="fg")
+
+	dfr[["Units"]] <- var.def$UnitsMD[match(thesevars, var.def$Variable)]
+	dfr[["Variable"]] <- var.def$Variable[match(thesevars, var.def$Variable)]
+	dfr[["Label"]] <- capitalize(var.def$label[match(thesevars, var.def$Variable)])
+
+	dfr <- dfr[dfr$N > 0,c("Variable",  "Label", "Units", "N", "Studies","Min","Median","Max")]
+	rownames(dfr) <- NULL
+
+	dfr
+}
