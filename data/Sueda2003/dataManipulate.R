9006bfcd668c14a3bd8da394902710ce0839e75e
Daniel Falster <daniel.falster@mq.edu.au>
2014-08-01 15:39:02 +1000

    Change directory names as requested by Ishihara

--- data/Sueda2003/dataManipulate.R
+++
@@ -1,9 +0,0 @@
-manipulate  <-  function(raw){
-
-	# Remove vine species
-	vines <- c("Trachelospermum asiaticum", "Akebia trifoliata", "Gardneria nutans", "Cocculus trilobus", "Wisteria floribunda", "Vitis saccharifera", "Hedera rhombea", "Ficus nipponica")
-
-	raw <- subset(raw, !(species %in% vines))
-
-	raw
-}
