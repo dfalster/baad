f8bb95dfe2978d61d61e7951692838b13a64a220
Daniel Falster <daniel.falster@mq.edu.au>
2014-08-01 15:12:24 +1000

    Modified files from data contributors

--- data/Coll2008/dataManipulate.r
+++ data/Coll2008/dataManipulate.r
@@ -1,6 +1,7 @@
 manipulate <- function(raw) {
   raw  <-  raw[is.na(raw$individual.leaf.mass.g),]
   raw[["stem.biomass.g"]] <- raw[["branch.biomass.g"]] + raw[["trunk.biomass.g"]]
+  raw[["ma.ilf"]] <- 1/raw[["specific.leaf.area.cm2/g"]]
   raw
 }
 
--- data/vanBreugel2011/dataManipulate.R
+++ data/vanBreugel2011/dataManipulate.R
@@ -1,11 +1,6 @@
 manipulate <- function(raw) {
 
   raw$grouping <- makeGroups(raw, "grouping")
-  
-  # Mistake in original processing
-  raw$h.c <- raw$h.t - raw$h.c
-  raw$c.d <- raw$h.t - raw$h.c
-  
-  
+
   raw
 }
