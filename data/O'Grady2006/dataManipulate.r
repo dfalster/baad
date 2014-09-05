f95676b23c6d54d85948b2909c567a40a6119f0f
Remko Duursma <remkoduursma@gmail.com>
2014-09-05 15:17:27 +1000

    O'Grady2006 - corrected obviously bad unit problem with subset of leaf area data

--- data/O'Grady2006/dataManipulate.r
+++ data/O'Grady2006/dataManipulate.r
@@ -5,9 +5,14 @@ manipulate <- function(raw) {
   raw[["m.rc"]]       <-  raw[["15 - 20"]] + raw[["20  + stump"]]
   raw$grouping        <-  makeGroups(raw, c("Harvested","plot"))
 
+  
+  # A problem with leaf areas; first 36 need to be multiplied by 10
+  # to get reasonable SLA values (RAD).
+  raw$X[1:36] <- raw$X[1:36] * 10
+  
   #Exclude data from zulu plot, these seem to be in different units
   raw  <-  raw[raw[["plot"]] != "zulu",]
   raw  <-  raw[!(raw[["plot"]] == 1 & raw[["seedling"]] ==16),]
-  raw
+  
 }
 
