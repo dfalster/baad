2456e5f5cca9936ef9b72c86a6497f5bde79be21
Remko Duursma <remkoduursma@gmail.com>
2014-09-05 15:27:17 +1000

    Rodriguez2003 - no longer read leaf area data, as these are based on very imprecise SLA estimates (many values way too low, cannot correct)

--- data/Rodriguez2003/dataManipulate.r
+++ data/Rodriguez2003/dataManipulate.r
@@ -1,5 +1,5 @@
 manipulate <- function(raw) {
-  raw[["Bark_Stemwood"]]                    <-  raw[["Bark"]] + raw[["Stemwood"]]
+  raw[["Bark_Stemwood"]] <-  raw[["Bark"]] + raw[["Stemwood"]]
   
   # To be consistent with publication Table 1.
   raw$Treatment <- paste0("T", raw$Treatment)
