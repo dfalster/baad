70f026e1820670d71972806f369453470009da4b
Remko Duursma <remkoduursma@gmail.com>
2014-09-16 13:01:49 +1000

    Wang1995 - fixed four obvious typos, and fixed m.st which included only stemwood for #152

--- data/Wang1995/dataManipulate.r
+++ data/Wang1995/dataManipulate.r
@@ -1,6 +1,17 @@
 manipulate <- function(raw) {
   
+  # Four points are massive outliers for stem mass;
+  # these are clearly 10 times too low (compared with all other mass components).
+  ii <- c(38,39,40,43)
+  raw[["STEMWOOD (kg)"]][ii] <- 10*raw[["STEMWOOD (kg)"]][ii]
   
+  # And recalculate total above.
+  raw[["TOTAL_ABOVE (kg)"]] <- raw[["STEMWOOD (kg)"]] + raw[["STEMBARK (kg)"]] + 
+    raw[["BRANCH (kg)"]] + raw[["LEAF (kg)"]]
+  
+  # m.st was wrongly just bole mass.
+  raw$mst <- raw[["TOTAL_ABOVE (kg)"]] - raw[["LEAF (kg)"]]
+    
   raw
 }
 
