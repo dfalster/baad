e8cc68d3f909a1d0d2f01ac6622f199b7c511e97
Remko Duursma <remkoduursma@gmail.com>
2014-09-16 14:04:09 +1000

    Wang1996 - m.st did not include bark or branches. For #152

--- data/Wang1996/dataManipulate.r
+++ data/Wang1996/dataManipulate.r
@@ -1,5 +1,9 @@
 manipulate <- function(raw) {
+
   
+  # m.st includes stem, bark, and branches
+  raw$mst <- raw[["STEMWOOD (kg)"]] + raw[["STEMBARK (kg)"]] + 
+    raw[["BRANCH (kg)"]]
   
   raw
 }
