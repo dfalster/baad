86ca6513e4e5098c29391ce3f54330d22982cff7
Remko Duursma <remkoduursma@gmail.com>
2014-09-16 14:31:34 +1000

    Coll2008 - SLA averaged by species and added to dataset. Closes #141

--- data/Coll2008/dataManipulate.r
+++ data/Coll2008/dataManipulate.r
@@ -1,7 +1,14 @@
 manipulate <- function(raw) {
+  
+  sladat <- raw[!is.na(raw$individual.leaf.mass.g),c("species","tree","specific.leaf.area.cm2/g")]
+  names(sladat)[3] <- "sla"
+  sladat <- aggregate(sla ~ species, FUN=mean, data=sladat)
+  
   raw  <-  raw[is.na(raw$individual.leaf.mass.g),]
+  raw <- merge(raw, sladat, by="species")
+  
   raw[["stem.biomass.g"]] <- raw[["branch.biomass.g"]] + raw[["trunk.biomass.g"]]
-  raw[["ma.ilf"]] <- 1/raw[["specific.leaf.area.cm2/g"]]
+  raw[["ma.ilf"]] <- 1/raw[["sla"]]
   raw
 }
 
