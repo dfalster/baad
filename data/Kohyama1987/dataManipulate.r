37bfdb3b7436ce47a1548cc884e9e1ce19695829
Daniel Falster <daniel.falster@mq.edu.au>
2014-09-16 14:37:49 +1000

    Kohyama1987: Add missing variable to matchColumns. Closes #148

--- data/Kohyama1987/dataManipulate.r
+++ data/Kohyama1987/dataManipulate.r
@@ -1,8 +1,10 @@
 manipulate <- function(raw) {
   raw[["leaf.mass"]]  <-  raw[["Wtl.g"]] + raw[["Wbl.g"]]
   raw[["m.st"]]       <-  raw[["Wts.g"]] + raw[["Wbs.g"]]
-  
-  
+  raw[["D.av"]]       <-  0.5*(raw[["D1.cm"]] + raw[["D2.cm"]])
+  # diameter measured at 1/10th of tree height, or on large trees with buttresses, above the buttress
+  raw[["h.bh"]] <- 0.1 *  raw[["H.cm"]]
+
   raw
 }
 
