0a785f95a3be4a8e4043602311dece9b5a5246a9
Remko Duursma <remkoduursma@gmail.com>
2014-09-16 13:59:07 +1000

    Santa-Regina1999 - fixed typo in total biomass, was not equal to leaf+trunk+branch biomass. For #152

--- data/SantaRegina1999/dataManipulate.r
+++ data/SantaRegina1999/dataManipulate.r
@@ -1,6 +1,12 @@
 manipulate <- function(raw) {
   raw[["Stem_biomass"]]  <-  raw[["Branch_biomass"]] + raw[["Trunk_biomass"]]
   
+  
+  # One glitch in total biomass, should be 151 not 161
+  # (check sum of leaf,branch,trunk with total biomass)
+  ii <- raw[["Total biomass"]] == 161
+  raw[["Total biomass"]][ii] <- 151
+  
   raw
 }
 
