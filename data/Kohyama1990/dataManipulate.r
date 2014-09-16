104728f18ddc8ec3b93a1f2023a485e0caf7956b
Daniel Falster <daniel.falster@mq.edu.au>
2014-09-16 14:14:42 +1000

    Add missing variable to matchColumns. Closes #149

--- data/Kohyama1990/dataManipulate.r
+++ data/Kohyama1990/dataManipulate.r
@@ -1,4 +1,8 @@
 manipulate <- function(raw) {
+
+  # diameter measured at 1/10th of tree height, or on large trees with buttresses, above the buttress
+  raw[["h.bh"]] <- 0.1 *  raw[["H.cm"]]
+
   raw$grouping  <-  makeGroups(raw, c("Year"))
   raw
 }
