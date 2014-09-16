d6b792491bfb5b25e03eafcc77ac3da1e0d7f4cc
Remko Duursma <remkoduursma@gmail.com>
2014-09-16 14:12:21 +1000

    SantaRegina1999 - recalculate m.so from component biomass, for #152

--- data/SantaRegina1999/dataManipulate.r
+++ data/SantaRegina1999/dataManipulate.r
@@ -2,10 +2,8 @@ manipulate <- function(raw) {
   raw[["Stem_biomass"]]  <-  raw[["Branch_biomass"]] + raw[["Trunk_biomass"]]
   
   
-  # One glitch in total biomass, should be 151 not 161
-  # (check sum of leaf,branch,trunk with total biomass)
-  ii <- raw[["Total biomass"]] == 161
-  raw[["Total biomass"]][ii] <- 151
+  # Couple glitches in total aboveground biomass, recalculate
+  raw[["Total biomass"]] <- raw[["Stem_biomass"]] + raw[["Leaf_biomass"]]
   
   raw
 }
