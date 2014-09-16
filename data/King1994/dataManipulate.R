2a65b92f182a151f9d796c4006df8b6692318c45
Daniel Falster <daniel.falster@mq.edu.au>
2014-09-16 13:59:30 +1000

    Add diameter infomration for King. Closes #144

--- data/King1994/dataManipulate.R
+++ data/King1994/dataManipulate.R
@@ -1,5 +1,6 @@
 manipulate <- function(raw) {
-  
+  # diameter measured at 1/10th of tree height, or on large trees with buttresses, above the buttress
+  raw[["h.bh"]] <- 0.1 *  raw[["ht1 (cm)"]]
   raw
 }
 
--- data/King1996/dataManipulate.r
+++ data/King1996/dataManipulate.r
@@ -1,6 +1,5 @@
 manipulate <- function(raw){
-  
-  
-  
+  # diameter measured at 1/10th of tree height, or on large trees with buttresses, above the buttress
+  raw[["h.bh"]] <- 0.1 *  raw[["Height (m)"]]
   raw
 }
