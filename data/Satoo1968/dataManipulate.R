7dad8c9ce0f70870b6b08729d97d040685196c73
Remko Duursma <remkoduursma@gmail.com>
2014-09-17 15:15:51 +1000

    Satoo1968 - Recalculate m.so, likely rounding error. Closes #152

--- data/Satoo1968/dataManipulate.R
+++ data/Satoo1968/dataManipulate.R
@@ -1,8 +1,11 @@
 manipulate  <-  function(raw){
 
-# Adding branch mass to stem mass
-raw$m.st <- raw$m.st + raw$m.br
+  # Adding branch mass to stem mass
+  raw$m.st <- raw$m.st + raw$m.br
 
+  # Recalculate m.so, it is slightly different from m.lf + m.st (likely rounding error)
+  raw$m.so <- raw$m.st + raw$m.lf
+  
 	# Remove vine species
 	vines <- c("Trachelospermum asiaticum", "Akebia trifoliata", "Gardneria nutans", "Cocculus trilobus", "Wisteria floribunda", "Vitis saccharifera", "Hedera rhombea", "Ficus nipponica")
 
