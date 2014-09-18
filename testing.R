80b184f8d203a421a1eb12d21d2bd862a0c1fdba
Daniel Falster <daniel.falster@mq.edu.au>
2014-09-18 12:28:26 +1000

    update testing script, based on update to dataMashR

--- testing.R
+++ testing.R
@@ -4,11 +4,4 @@ library(dataMashR)
 # for debug
 # options(warn=1, error=recover)
 
-context("Check package setup")
-
-validateConfig("config")
-
-context("Check datasets")
-
-for(d in dir("data"))
-	validateStudy(d, "config")
+checkPackage(".")
