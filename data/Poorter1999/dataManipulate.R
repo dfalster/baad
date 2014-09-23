c687723f4b490595a01a8d0dd8e4aaa688ef7934
Remko Duursma <remkoduursma@gmail.com>
2014-09-23 15:15:10 +1000

    Poorter1999 - fixed wrong crown depth. Closes #167

--- data/Poorter1999/dataManipulate.R
+++ data/Poorter1999/dataManipulate.R
@@ -4,7 +4,12 @@ manipulate <- function(raw) {
   raw[["d.cr"]] <- sqrt(raw[["d.cr"]] * raw[["d.cr2"]])
   
   # Set zero and negative crown depth to NA
-  raw$c.d[raw$c.d <= 0] <- NA
+  ii <- which(raw$c.d <= 0)
+  raw$c.d[ii] <- NA
+  
+  # When crown depth > h.t, set to h.t
+  ii <- which(raw$c.d > raw$h.t)
+  raw$c.d[ii] <- raw$h.t[ii]
   
   raw
 }
