5fe2f18a0d1e7684a79badf1d1a2b9085ed24041
Remko Duursma <remkoduursma@gmail.com>
2014-09-23 14:35:11 +1000

    vanBreugel2011 - zero crown length set to NA

--- data/vanBreugel2011/dataManipulate.R
+++ data/vanBreugel2011/dataManipulate.R
@@ -2,5 +2,10 @@ manipulate <- function(raw) {
 
   raw$grouping <- makeGroups(raw, "grouping")
 
+  ii <- which(raw$h.c == raw$h.t)
+  raw$h.c[ii] <- NA
+  ii <- which(raw$c.d == 0)
+  raw$c.d[ii] <- NA
+  
   raw
 }
