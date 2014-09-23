66b2dc7a9fa7bd589e7940fe130d4c72b0426bf5
Remko Duursma <remkoduursma@gmail.com>
2014-09-23 15:06:07 +1000

    Claveau2002 - fixed wrong h.c and c.d for #167

--- data/Claveau2002/dataManipulate.r
+++ data/Claveau2002/dataManipulate.r
@@ -4,8 +4,10 @@ manipulate <- function(raw) {
   # Zero c.d
   raw$c.d[raw$c.d == 0] <- NA
 
-  # same
-  raw$h.c[raw$h.t == raw$h.c] <- NA
+  # When crown depth > height, set to height
+  ii <- which(raw$c.d > raw$h.t)
+  raw$c.d[ii] <- raw$h.t[ii]
+  raw$h.c[ii] <- 0
   
   
 raw
