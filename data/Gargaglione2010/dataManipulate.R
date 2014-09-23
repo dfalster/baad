695af545b0aa98178268ce36edd9e2af13723e5e
Remko Duursma <remkoduursma@gmail.com>
2014-09-23 13:57:29 +1000

    Gargaglione2010 - Inf values for n.sb and n.sh removed for #167

--- data/Gargaglione2010/dataManipulate.R
+++ data/Gargaglione2010/dataManipulate.R
@@ -21,16 +21,21 @@ manipulate <- function(raw) {
 
   raw$coarse_root_mass  <-  raw[["medium roots mass"]] + raw[["coarse roots mass"]]
 
+  # zero value in total sapwood, set to NA
+  nm <- "total sapwood"
+  raw[[nm]][raw[[nm]] == 0] <- NA
+  
   raw[["total N sapwood"]]    <-  (raw[["total N sapwood"]]/1000)/raw[["total sapwood"]]
   raw[["Total N bark"]]       <-  (raw[["Total N bark"]]/1000)/raw[["Total bark mass"]]
   raw[["total N heartwood"]]  <-  (raw[["N heartwood"]]/1000)/raw[["total heartwood"]]
   raw[["N fine roots"]]       <-  (raw[["N fine roots"]]/1000)/raw[["fine roots mass"]]
   raw$NcoarseRoots            <-  ((raw[["N medium roots"]] + raw[["N coarse roots"]])/1000)/raw$coarse_root_mass
 
-  # zero value in total sapwood, set to NA
-  nm <- "total sapwood"
-  raw[[nm]][raw[[nm]] == 0] <- NA
-
+  # Dividing by zero gives Inf.
+  raw[["total N heartwood"]][!is.finite(raw[["total N heartwood"]])] <- NA
+  raw[["total N sapwood"]][!is.finite(raw[["total N sapwood"]])] <- NA
+  raw[["Total N bark"]][!is.finite(raw[["Total N bark"]])] <- NA
+  
   # zero d_bh is NA (note: zero dbh is possible when h.t < 1.4 or 1.3).
   d0 <- raw[["DBH (cm)"]] == 0
   raw[["DBH (cm)"]][d0] <- NA
