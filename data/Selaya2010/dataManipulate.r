4201515c272bff0b36dbfbf973bef3256a1b03e8
dbarneche <diego.barneche@ymail.com>
2014-09-26 11:19:43 +1000

    merged Selaya studies (including family info, status, typos and data pre-manipulatation) into 2010 as this paper seems to use all datasets at once. Closes #172

--- data/Selaya2010/dataManipulate.r
+++ data/Selaya2010/dataManipulate.r
@@ -1,20 +1,24 @@
 manipulate <- function(raw) {
-  raw        <-  raw[raw[["reference"]]=="Selaya et al. (2008) Journal of Ecology 96: 1211-1221; Selaya & Anten (2010) Ecology 91:1102-1113",]
+  
+  # Row 163 is a massive outlier (m.st, but not clear which vars are wrong)
+  raw <- raw[-163,]
+
   raw[["light"]]  <-  raw[["light"]]/55*100
+  raw$n.lf        <-  raw$n.lf*raw$a.lf/raw$m.lf
 
   # Fix grouping. This info is also in 'age', but repeat it here to be safe.
-  names(raw)[names(raw) == "group..7."] <- "grouping"
-  raw$grouping <- makeGroups(raw, "grouping")
+  names(raw)[names(raw) == "group..7."] <-  "grouping"
+  raw$grouping                          <-  makeGroups(raw, "grouping")
   
   # Massive outlier : wrong stem area measurement
   raw$a.st[raw$a.st > 0.2] <- NA
   
   # Zero
-  raw$a.st[raw$a.st == 0] <- NA
+  raw$a.st[raw$a.st == 0]  <- NA
   
   # Mistake in original processing and a zero fix
-  raw$h.c <- raw$h.t - raw$h.c
-  raw$h.c[raw$h.c > raw$h.t] <- NA
+  raw$h.c                    <-  raw$h.t - raw$h.c
+  raw$h.c[raw$h.c > raw$h.t] <-  NA
   
   raw
 }
