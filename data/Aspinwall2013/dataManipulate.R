b54bdfe9be1b64917650c44968364d6aa5b03b2d
Remko Duursma <remkoduursma@gmail.com>
2014-10-29 21:22:25 +1100

    Aspinwall2013 - updated methods in dataMatchcolumns, and added info in datamanipulate

--- data/Aspinwall2013/dataManipulate.R
+++ data/Aspinwall2013/dataManipulate.R
@@ -1,6 +1,11 @@
 manipulate <- function(raw){
 
-
+  # From author's notes:
+  # identifies the genetic 'level' of the genotypes in the study; 
+  # 'cl' = clone, 'fs' = full-sib family, 'hs' = half-sib family
+  # indentity of each genotype, 
+  # c1, c2, and c3 represent clone 1, clone 2 and clone 3, 
+  # fs1, fs2 and fs3 = fullsib family 1, fullsib family 2, and full-sib family 3 etc.
   raw$grouping  <-  makeGroups(raw, c("genlev","genotype", "rep"))
 
   raw$totroot <- with(raw, coarserootdw + taprootdw)
