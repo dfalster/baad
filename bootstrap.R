82b0b1c832e9fcfd7c1d1e6cf42f7c8b97e5d323
Rich FitzJohn <rich.fitzjohn@gmail.com>
2014-11-19 18:20:42 +1100

    Basic maker approach working

---
+++ bootstrap.R
@@ -0,0 +1,17 @@
+#!/usr/bin/env Rscript
+library(whisker)
+
+read_templates <- function(path) {
+  files <- dir(path, pattern=glob2rx("*.whisker"))
+  templates <- lapply(file.path(path, files), readLines)
+  names(templates) <- tools::file_path_sans_ext(files)
+  templates
+}
+
+templates <- read_templates("bootstrap")
+
+study_names <- dir("data")
+vals <- list(study_names=iteratelist(study_names, value="study_name"))
+
+str <- whisker.render(templates$maker_data.yml, vals, templates)
+writeLines(str, "maker_data.yml")
