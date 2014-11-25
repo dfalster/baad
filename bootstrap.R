0954685d88c1ad911595100100a1635b947dc909
Daniel Falster <daniel.falster@mq.edu.au>
2014-11-25 13:56:52 +1100

    Move bootstrap files into config

--- bootstrap.R
+++ bootstrap.R
@@ -4,8 +4,8 @@ library(whisker)
 study_names <- dir("data")
 vals <- list(study_names=iteratelist(study_names, value="study_name"))
 
-str <- whisker.render(readLines("bootstrap/maker_data.yml.whisker"), vals)
+str <- whisker.render(readLines("config/maker_data.yml.whisker"), vals)
 writeLines(str, "maker_data.yml")
 
-str <- whisker.render(readLines("bootstrap/maker_reports.yml.whisker"), vals)
+str <- whisker.render(readLines("config/maker_reports.yml.whisker"), vals)
 writeLines(str, "maker_reports.yml")
