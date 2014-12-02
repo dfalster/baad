2814c4e0c52ac276922f0a49f3a730fd84589ead
Daniel Falster <daniel.falster@mq.edu.au>
2014-12-03 10:29:56 +1100

    move evil function in with others

--- R/evil.R
+++
@@ -1,8 +0,0 @@
-# Paste together list of var_names and their values, used for
-# aggregating varnames into 'grouping' variable NOTE: Used in
-# dataManipulate.R
-makeGroups <- function(data, var_names) {
-  apply(cbind(data[, var_names]), 1, function(x)
-        ## jsonlite::toJSON(as.list(x), auto_unbox=TRUE)
-        paste(var_names, "=", x, collapse = "; "))
-}
--- R/support.R
+++ R/support.R
@@ -58,3 +58,12 @@ capitalize <- function (string) {
 write_csv <- function(data, filename) {
   write.csv(data, filename, row.names=FALSE)
 }
+
+# Paste together list of var_names and their values, used for
+# aggregating varnames into 'grouping' variable NOTE: Used in
+# dataManipulate.R
+makeGroups <- function(data, var_names) {
+  apply(cbind(data[, var_names]), 1, function(x)
+        ## jsonlite::toJSON(as.list(x), auto_unbox=TRUE)
+        paste(var_names, "=", x, collapse = "; "))
+}
