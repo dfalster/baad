50875ceefbf14eab078298ad99908f7b42b39e5e
Remko Duursma <remkoduursma@gmail.com>
2016-06-20 14:56:38 +1000

    Monserud1999 - fixed wrong leaf areas. Closes #226

--- data/Monserud1999/dataManipulate.R
+++ data/Monserud1999/dataManipulate.R
@@ -4,6 +4,12 @@ manipulate <- function(raw) {
   # h_c was crown length, should be height to crown base
   raw$h_c <- with(raw, h_t - h_c)
   
+  # Included leaf areas were mistakenly converted to all-sided area. Re-convert here.
+  raw$a_lf[raw$species == "PIMO"] <- raw$a_lf[raw$species == "PIMO"] / (1.18*2)
+  raw$a_lf[raw$species == "PIPO"] <- raw$a_lf[raw$species == "PIPO"] / (1.18*2)
+  raw$a_lf[raw$species == "PSME"] <- raw$a_lf[raw$species == "PSME"] / (1.19*2)
+  
+  
   raw
 }
 
