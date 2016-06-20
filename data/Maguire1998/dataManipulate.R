fdae3ee7ebfb4cae7d1b8dd73e13db9afd26d4c7
Remko Duursma <remkoduursma@gmail.com>
2016-06-20 15:06:13 +1000

    Maguire1998 - just like issue #226, leaf area was mistakenly converted to all-sided

--- data/Maguire1998/dataManipulate.R
+++ data/Maguire1998/dataManipulate.R
@@ -4,6 +4,9 @@ manipulate <- function(raw) {
   # h_c was crownlength, should be height to crown base
   raw$h_c <- with(raw, h_t - h_c)
   
+  # Leaf area was in error: already converted to all-sided area
+  raw$a_lf <- raw$a_lf / (2 * 1.18)
+  
   raw
 }
 
